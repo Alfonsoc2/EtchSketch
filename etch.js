dfbff12b290c8a34822bfde7f7ac3e614e98e497
alfonsoc2 <alfonsocenteno2@gmail.com>
2024-11-06 20:40:56 -0700

    added min height to css to ensure box is always visibe finished coding divs for boxes in js

--- etch.js
+++ etch.js
@@ -1,1 +1,8 @@
-// placeholder for js code
+const container = document.getElementById('grid-container');
+
+document.createElement('div')
+const totalSquares = 16 * 16;
+for (let i = 0; i < totalSquares; i++) {
+    const newDiv = document.createElement('div');
+    container.appendChild(newDiv);
+}
